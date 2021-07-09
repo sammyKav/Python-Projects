9902ef00085c296664677b284ab4eae12fef8421
sammyKav <skavkewi@icloud.com>
2021-07-09 11:04:29 -0700

    Python_class_inheritance_Assignment

--- Class_Assignment.py
+++ Class_Assignment.py
@@ -1,5 +1,5 @@
 
-class Facility:
+class Facility:  # creating facilities for a fictional company. All building have below attributes
     name = 'No Name Provided'
     country ='US'
     city = ''
@@ -8,7 +8,7 @@ class Facility:
     max_occupancy = 1
     phone = ''
 
-class DC(Facility):
+class DC(Facility): # has attributes unqiue to a dc. 
     dock_doors = 1
     inv_ailse = 0
     hazmat_ailse = 0
@@ -16,7 +16,7 @@ class DC(Facility):
     DC_volume_rank = 1
     
 
-class Store(Facility):
+class Store(Facility): # has attributes unique to a retail store. 
     checkout_lines = 1
     merch_ailes = 0
     dock_door = 0
