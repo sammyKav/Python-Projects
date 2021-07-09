3e1277e6b11e04f976f65ebcaaec319c63bb77be
sammyKav <skavkewi@icloud.com>
2021-07-09 11:02:16 -0700

    Python_class_inheritance_Assignment

---
+++ Class_Assignment.py
@@ -0,0 +1,29 @@
+
+class Facility:
+    name = 'No Name Provided'
+    country ='US'
+    city = ''
+    state = ''
+    sqrFt = 0
+    max_occupancy = 1
+    phone = ''
+
+class DC(Facility):
+    dock_doors = 1
+    inv_ailse = 0
+    hazmat_ailse = 0
+    DC_manger = ''
+    DC_volume_rank = 1
+    
+
+class Store(Facility):
+    checkout_lines = 1
+    merch_ailes = 0
+    dock_door = 0
+    store_manger = ''
+    sales_volume_rank = 1
+    
+    
+
+
+    
