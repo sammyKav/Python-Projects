e7d129273485a6b1746941bc7427c2ccaf46f0c5
sammyKav <skavkewi@icloud.com>
2021-07-12 16:08:20 -0700

    Python_private_and_protected_Assignment

---
+++ Private_Protected.py
@@ -0,0 +1,13 @@
+#Protected Class
+class Employee:
+    def __init__(self,name,job_title,num_citations):
+        self._name = name #protected can be accessed but not advised. 
+        self.job_title = job_title #public
+        self.__num_citations = num_citations # private gives attribute error
+
+
+
+e1 = Employee("Sam Kavkewitz","CEO",183)
+     
+print(e1.__num_citations) # attribute error
+
